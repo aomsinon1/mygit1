90c637670841344fa37bd93299f10ce8003160ee
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2024-01-17 11:57:39 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("การสร้างเว็บด้วยpython")
-st.image("413333963_394679022959881_6016511511527722084_n")
+st.image("aaa.png")
 st.header("การนำเสนอข้อมูลกราฟด้วยpython")
